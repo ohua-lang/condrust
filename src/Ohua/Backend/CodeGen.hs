ae56ad2625fbd578a60905f81eed3f291c526d2f
Sebastian Ertel <sebastian.ertel@gmail.com>
2018-11-28 13:45:30 +0100

    Initialization

---
+++ src/Ohua/Backend/CodeGen.hs
@@ -0,0 +1,9 @@
+module Ohua.Backend.CodeGen where
+
+import Universum
+
+import qualified Ohua.DFGraph as OC
+import qualified One4All.Lang as O4A
+
+genCode :: OC.OutGraph -> O4A.Expr
+genCode = undefined
