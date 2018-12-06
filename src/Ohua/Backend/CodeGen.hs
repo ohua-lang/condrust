112353f631ab8b40a0c4069f872161cae7ea3614
Felix Wittwer <dev@felixwittwer.de>
2018-12-06 16:42:05 +0100

    Generate out arc

--- src/Ohua/Backend/CodeGen.hs
+++ src/Ohua/Backend/CodeGen.hs
@@ -3,6 +3,7 @@ module Ohua.Backend.CodeGen where
 import Universum
 
 import qualified Ohua.DFGraph as OC
+import qualified Ohua.Types as OT
 import qualified One4All.Lang as O4A
 
 genCode :: OC.OutGraph -> O4A.Expr
@@ -15,17 +16,34 @@ genArcs :: OC.OutGraph -> O4A.Expr
 genArcs (OC.OutGraph operators arcs _) =
     let localArcs = filter isNotEnvArc arcs
      -- FIXME Why is this supposed to be a monad?
-     in do outArcs <- [genOutArcVariable x operators | x <- localArcs]
+     in do outArcs <- [genOutArcVariable x | x <- localArcs]
            inArcs <- [genInArcVariable x | x <- localArcs]
            undefined
-  -- continue here! generierung der arcs schreiben (ich glaube $x ist für variableneinsetzung)
+  -- TODO: continue here! write arc generation
 
 isNotEnvArc :: OC.Arc envExpr -> Bool
 isNotEnvArc (OC.Arc _ (OC.LocalSource _)) = True
 isNotEnvArc (OC.Arc _ (OC.EnvSource _)) = False
 
-genOutArcVariable :: OC.Arc envExpr -> [OC.Operator] -> O4A.Var
-genOutArcVariable = undefined
+genOutArcVariable :: OC.Arc envExpr -> O4A.Var
+genOutArcVariable (OC.Arc target src) =
+    let out_port = genOutArcVariable' (getOpId src) (getOutIdxFromSrc src)
+     in let in_port = genInArcVariable (OC.Arc target src)
+         in case in_port of
+                O4A.V s -> O4A.V (show out_port ++ "__" ++ s)
+                O4A.AV _ -> undefined
+
+-- TODO: This originally used the respective operator to check whether the arc originated from a SFN or a dataflow operator. However, this destinction does not (yet) exist in the DFGraph data structure and hence can not be used to verify correctness.
+genOutArcVariable' :: OT.FnId -> Int -> String
+genOutArcVariable' op idx =
+    let computed_idx =
+            case idx of
+                idx
+                    | idx == (-1) -> 0
+                idx
+                    | idx >= 0 -> idx
+                otherwise -> undefined -- error "Invalid operator index: " ++ show idx
+     in "sf_" ++ show op ++ "_out_" ++ show computed_idx
 
 genInArcVariable :: OC.Arc envExpr -> O4A.Var
 genInArcVariable (OC.Arc (OC.Target op (-1)) _) =
@@ -34,6 +52,14 @@ genInArcVariable (OC.Arc (OC.Target op idx) _)
     | idx > -2 = O4A.V ("sf_" ++ show op ++ "_in_" ++ show idx :: String)
 
 -- utilities
+getOpId :: OC.Source envExpr -> OT.FnId
+getOpId (OC.LocalSource (OC.Target id _)) = id
+getOpId (OC.EnvSource id) = undefined
+
+getOutIdxFromSrc :: OC.Source envExpr -> Int
+getOutIdxFromSrc (OC.LocalSource (OC.Target _ idx)) = idx
+getOutIdxFromSrc (OC.EnvSource idx) = undefined
+
 isFinalBinding :: O4A.Var -> Bool
 isFinalBinding (O4A.V "toBeCompleted") = True
 isFinalBinding _ = False
