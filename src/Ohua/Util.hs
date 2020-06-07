880364e12241ac0b528b1a9009cbcac54cc33183
Sebastian Ertel <sebastian.ertel@gmail.com>
2020-06-07 22:16:52 +0200

    adding missing import statement

--- src/Ohua/Util.hs
+++ src/Ohua/Util.hs
@@ -3,6 +3,8 @@ module Ohua.Util
     intentionally_not_implemented
     ) where
 
+import Universum
+
 intentionally_not_implemented :: a
 intentionally_not_implemented =
     error "This is intentionally not implemented, don't use it!"
