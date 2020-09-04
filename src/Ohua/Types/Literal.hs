bad97aee9aaf1fefe0cc77ae562cb040e88674fb
Sebastian Ertel <sebastian.ertel@gmail.com>
2020-09-04 11:52:09 +0200

    missing boolean literal.

--- src/Ohua/Types/Literal.hs
+++ src/Ohua/Types/Literal.hs
@@ -10,6 +10,7 @@ import Ohua.Types.Reference
 -- | Literals of kinds we expect any host language to support
 data Lit
     = NumericLit !Integer -- ^ an integer literal
+    | BoolLit Bool -- ^ a boolean literal
     | UnitLit -- ^ aka @()@
     | EnvRefLit !HostExpr -- ^ A reference to some value from the environment
     | FunRefLit FunRef -- ^ Reference to an external function
