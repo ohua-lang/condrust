dc8a58a7a5f7ca5d8ae74ca0ebb43d9a49653c1d
Sebastian Ertel <sebastian.ertel@gmail.com>
2021-02-04 20:27:26 +0100

    I finally got my hands direty on singletons and managed to get a type-safe version of RecurFun.

--- src/Ohua/Types/Vector.hs
+++ src/Ohua/Types/Vector.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE InstanceSigs #-}
 module Ohua.Types.Vector where
 
-import Universum hiding (Nat, toList)
+import Universum hiding (Nat, toList, map)
 import Data.List.NonEmpty (NonEmpty(..))
+import Data.Singletons
 
 data Nat where
     Zero :: Nat 
@@ -13,21 +17,58 @@ deriving instance Show Nat
 
 data Vec (n::Nat) a where
     VNil :: Vec 'Zero a
-    VCons :: a -> Vec n a -> Vec ('Succ n) a
+    (:>) :: a -> Vec n a -> Vec ('Succ n) a
 
 deriving instance (Show a) => Show (Vec n a)
 
+-- computed by $(genSingletons [’’Nat])
+-- Sing is an open type family (as opposed to a closed type family declared with a GADT)
+-- data instance Sing (a :: Nat) where
+--     SZero :: Sing 'Zero
+--     SSucc :: (SingI n) => Sing n -> Sing ('Succ n)
+-- type SNat (n ::Nat) = Sing n
+
+-- Resources:
+-- https://blog.jle.im/entry/introduction-to-singletons-2.html
+-- https://github.com/goldfirere/singletons/tree/01fa5635d8e4db0be8d9537a44d777541969354b#arrows-nat-symbol-and-literals
+-- https://github.com/goldfirere/singletons/issues/76
+-- Singletons paper: https://cs.brynmawr.edu/~rae/papers/2012/singletons/paper.pdf (some listings are out-dated - Sing is now a type family)
+
+data SNat :: Nat -> Type where
+    SZero :: SNat 'Zero
+    SSucc :: SNat n -> SNat ('Succ n)
+
+type instance Sing = SNat
+
+instance SingKind Nat where
+    type Demote Nat = Nat
+
+    fromSing :: Sing (s :: Nat) -> Nat
+    fromSing SZero = Zero
+    fromSing (SSucc n) = Succ $ fromSing n
+
+    toSing :: Nat -> SomeSing Nat
+    toSing Zero = SomeSing SZero
+    toSing (Succ n) = case toSing n of
+                        (SomeSing n') -> SomeSing $ SSucc n'
+
+toNonEmpty :: Vec ('Succ n) a -> NonEmpty a
+toNonEmpty (x :> xs) = x :| toList xs
+
 toList :: Vec n a -> [a]
 toList VNil = []
-toList (VCons a rest) = a : toList rest
+toList (a:>rest) = a : toList rest
 
-toNonEmpty :: (n ~ 'Succ m) => Vec n a -> NonEmpty a
-toNonEmpty (VCons a rest) = a :| toList rest
+fromList :: SNat n -> [a] -> Vec n a
+fromList s xs = 
+    case (s,xs) of 
+        (SZero,[]) -> VNil
+        (SSucc s', a:rest) -> a :> fromList s' rest
 
--- fromNonEmpty :: (n ~ 'Succ m) =>  NonEmpty a -> Vec n a
--- fromNonEmpty (a :| rest) = VCons a $ fromList rest
+map :: (a -> b) -> Vec n a -> Vec n b
+map f VNil = VNil
+map f (x :> xs) = f x :> map f xs
 
--- fromList :: [a] -> Vec n a
--- fromList xs = case xs of 
---                 [] -> VNil
---                 (a:rest) -> VCons a $ fromList rest
+nlength :: [a] -> Nat
+nlength [] = Zero
+nlength (x:xs) = Succ $ nlength xs
