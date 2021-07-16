134a85cddd5b954cf03d0578cb65a1771acaf57b
Felix Wittwer <dev@felixwittwer.de>
2021-07-16 12:53:49 +0200

    Simplify dispatch pass & fix regression

--- core/src/Ohua/Core/DFLang/Passes/DispatchInsertion.hs
+++ core/src/Ohua/Core/DFLang/Passes/DispatchInsertion.hs
@@ -14,16 +14,6 @@ insertDispatch (Let app cont) =
         (cont', outputs') <- foldMapOutData cont outputs
         cont'' <- insertDispatch cont'
         return $ Let (PureDFFun outputs' fn inputs) cont''
-        -- let sizeChan = last $ outBnds outputs
-        -- -- run the rename
-        -- (cont', sizeChannels) <- renameChannels (cont, []) sizeChan
-        -- -- if length of bindings is 1, don't do anything, else, change the outputs and return t
-        -- if length sizeChannels == 1
-        --   then Let app <$> insertDispatch cont
-        --   else do
-        --     outputs' <- replaceSizeWithDispatch outputs sizeChannels
-        --     cont'' <- insertDispatch cont'
-        --     return $ Let (PureDFFun outputs' fn inputs) cont''
     _ -> Let app <$> insertDispatch cont
 insertDispatch v = pure v
 
@@ -35,7 +25,10 @@ foldMapOutData ::
   m (NormalizedDFExpr ty, OutData b)
 foldMapOutData expr (Direct bnd@(DataBinding _)) = do
   (cont', bnds') <- renameChannels (expr, []) (unwrapABnd bnd)
-  return (cont', (Dispatch $ NE.fromList bnds'))
+  case bnds' of
+    [] -> throwError "Internal compiler error: Renaming a channel yielded no channel name."
+    [x] -> return (cont', (Direct x))
+    _ -> return (cont', (Dispatch $ NE.fromList bnds'))
 foldMapOutData expr ob@(Direct (StateBinding _)) = return (expr, ob)
 foldMapOutData expr (Destruct bnds) = do
   (cont', bnds') <- foldrM g (expr, []) bnds
@@ -45,7 +38,7 @@ foldMapOutData expr (Destruct bnds) = do
     g bnd (exp, currentBinds) = do
       (expr', currentBinds') <- foldMapOutData exp bnd
       return (expr', (currentBinds' : currentBinds))
-foldMapOutData expr (Dispatch bnds@((DataBinding _):|_)) = do
+foldMapOutData expr (Dispatch bnds@((DataBinding _) :| _)) = do
   (cont', bnds') <- foldM f (expr, []) bnds
   return (cont', (Dispatch $ NE.fromList bnds'))
   where
@@ -53,8 +46,7 @@ foldMapOutData expr (Dispatch bnds@((DataBinding _):|_)) = do
     f (exp, currentBinds) bnd = do
       (expr', currentBinds') <- renameChannels (exp, []) (unwrapABnd bnd)
       return (expr', (currentBinds ++ currentBinds'))
-foldMapOutData expr ob@(Dispatch ((StateBinding _):|_)) = return (expr, ob)
-
+foldMapOutData expr ob@(Dispatch ((StateBinding _) :| _)) = return (expr, ob)
 
 -- takes the smap body, the binding to the size channel, whether the binding has been seen before and the replaced bindings
 renameChannels ::
@@ -63,35 +55,27 @@ renameChannels ::
   (NormalizedDFExpr ty, [ABinding 'Data]) ->
   Binding ->
   m (NormalizedDFExpr ty, [ABinding 'Data])
-renameChannels (e@(Let app cont), newBinds) bnd =
-  case app of
-    (PureDFFun out fn inp)
-      -- end condition
-      | fn == collect -> pure (e, newBinds)
-      -- TODO(feliix42): Do we need to catch multiple occurences of the same size channel here? Currently that can't happen, but who knows what will happen.
-      | otherwise ->
-        if elem bnd $ extractBndsFromInputs $ NE.toList inp
-          then -- rewrite
-
-            if length newBinds == 0
-              then do
-                (cont', newBinds') <- renameChannels (cont, [DataBinding bnd]) bnd
-                return (Let app cont', newBinds')
-              else do
-                -- create a new binding
-                newBnd <- DataBinding <$> generateBindingWith bnd
-                let inp' = map (replaceInput bnd newBnd) inp
-                let newBinds' = newBnd : newBinds
-                (cont', newBinds'') <- renameChannels (cont, newBinds') bnd
-                return (Let (PureDFFun out fn inp') cont', newBinds'')
-          else do
-            -- no match, continue
-            (cont', newBinds') <- renameChannels (cont, newBinds) bnd
-            return (Let app cont', newBinds')
-    -- TODO(feliix42): Actually handle other function types if necessary
-    _ -> do
-      (cont', newBinds') <- renameChannels (cont, newBinds) bnd
-      return (Let app cont', newBinds')
+renameChannels ((Let app@(PureDFFun out fn inp) cont), newBinds) bnd
+  | elem bnd $ extractBndsFromInputs $ NE.toList inp =
+    -- rewrite
+    do
+      -- create a new binding
+      newBnd <- DataBinding <$> generateBindingWith bnd
+      let inp' = map (replaceInput bnd newBnd) inp
+      let newBinds' = newBnd : newBinds
+      (cont', newBinds'') <- f fn (cont, newBinds') bnd
+      return (Let (PureDFFun out fn inp') cont', newBinds'')
+  | otherwise = do
+    -- no match, continue
+    (cont', newBinds') <- f fn (cont, newBinds) bnd
+    return (Let app cont', newBinds')
+  where
+    f fn acc bnd
+      | fn == collect = pure acc
+      | otherwise = renameChannels acc bnd
+renameChannels ((Let app cont), newBinds) bnd = do
+  (cont', newBinds') <- renameChannels (cont, newBinds) bnd
+  return (Let app cont', newBinds')
 renameChannels ((Var _), _) _ = throwError $ "Invariant broken: Found an smap not delimited by a collect"
 
 replaceInput :: Binding -> ABinding 'Data -> DFVar 'Data a -> DFVar 'Data a
@@ -100,10 +84,3 @@ replaceInput old newBind var = case var of
     | unwrapABnd bnd == old -> (DFVar t newBind)
     | otherwise -> var
   (DFEnvVar _ _) -> var
-
--- replaceSizeWithDispatch :: OutData b -> [ABinding 'Data] -> m (OutData b)
--- replaceSizeWithDispatch (Destruct binds) newBinds =
---   let lst' = NE.init binds
---    in pure $ Destruct $ NEE.snoc (NE.fromList lst') $ Dispatch $ NE.fromList newBinds
--- -- TODO(feliix42): @Sebastian how do you throw the error correctly here?
--- replaceSizeWithDispatch _ _ = throwError "Found unexpected OutData format in smap output."
