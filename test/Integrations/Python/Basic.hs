4c82b68aace58aa0c9d8415989e193af238de7b9
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2021-12-10 17:37:21 +0100

    actual python subscript tests

--- test/Integrations/Python/Basic.hs
+++ test/Integrations/Python/Basic.hs
@@ -11,7 +11,30 @@ import qualified Integrations.Python.TestDataOutput as Expect
 spec :: Spec
 spec =
     describe "Basics" $ do
+        it "Simple function call" $
+            (showCode "Compiled: " =<< compileCode Input.callAFunction) >>=
+            (\compiled -> do
+                expected <- showCode "Expected:" Expect.callAFunction
+                compiled `shouldBe` expected)
+
+        it "Assignment no Return" $
+            (showCode "Compiled: " =<< compileCode Input.noReturn) >>=
+            (\compiled -> do
+                expected <- showCode "Expected:" Expect.noReturn
+                compiled `shouldBe` expected)
         
+        it "Only 'return' should be the same as no 'return' " $
+            (showCode "Compiled: " =<< compileCode Input.emptyReturn) >>=
+            (\compiled -> do
+                expected <- showCode "Expected:" =<< compileCode Input.noReturn
+                compiled `shouldBe` expected)
+
+        it "Only 'return' should be the same as 'return None' " $
+            (showCode "Compiled: " =<<  compileCode Input.emptyReturn) >>=
+            (\compiled -> do
+                expected <- showCode "Expected:" =<< compileCode Input.noneReturn
+                compiled `shouldBe` expected)
+                
         it "Assignment binary Operation of integer literals" $
             (showCode "Compiled: " =<< compileCode Input.assignBinOp) >>=
             (\compiled -> do
@@ -53,12 +76,6 @@ spec =
             (\compiled -> do
                 expected <- showCode "Expected:" Expect.nestedCompose
                 compiled `shouldBe` expected) 
-
-        it "Assignments, Call function, Assign,  Return Var" $
-            (showCode "Compiled: " =<< compileCode Input.assignCallAssignReturn) >>=
-            (\compiled -> do
-                expected <- showCode "Expected:" Expect.assignCallAssignReturn
-                compiled `shouldBe` expected)
 
         it "Algo with params" $
             (showCode "Compiled: " =<< compileCode Input.algoWithParams) >>=
@@ -115,96 +132,18 @@ spec =
                 expected <- showCode "Expected:" Expect.tupleArgumentCall
                 compiled `shouldBe` expected)
 
-        
-        
-
-        
-{-  TOFIX 
-        {-
-        uncaught exception: ErrorCall
-       Internal Error: Tried running substitution on sending Binding "x" with not yet supported task expression: Lit UnitLit
-        -}
-        it "Simple function call" $
-            (showCode "Compiled: " =<< compileCode Input.callAFunction) >>=
+        it "Assign to subscript" $
+            (showCode "Compiled: " =<< compileCode Input.assignToSubscript) >>=
             (\compiled -> do
-                expected <- showCode "Expected:" Expect.callAFunction
+                expected <- showCode "Expected:" Expect.assignToSubscript
                 compiled `shouldBe` expected)
-
-
-         {-
-        uncaught exception: ErrorCall
-        Internal Error: Tried running substitution on sending Binding "x" with not yet supported task expression: Lit UnitLit
-        CallStack (from HasCallStack):
-             error, called at src/Ohua/Backend/Normalize.hs:83:12 in ohua-backend-0.1.0.0-IU5LaHcdXoz65g8YnoPRFA:Ohua.Backend.Normalize
-        -}
-        it "Assignment no Return" $
-            (showCode "Compiled: " =<< compileCode Input.noReturn) >>=
+               
+        it "Assign subscript to var" $
+            (showCode "Compiled: " =<< compileCode Input.assignSubscript) >>=
             (\compiled -> do
-                expected <- showCode "Expected:" Expect.noReturn
+                expected <- showCode "Expected:" Expect.assignSubscript
                 compiled `shouldBe` expected)
         
-        {-
-        uncaught exception: ErrorCall
-        Internal Error: Tried running substitution on sending Binding "x" with not yet supported task expression: Lit UnitLit
-        CallStack (from HasCallStack):
-             error, called at src/Ohua/Backend/Normalize.hs:83:12 in ohua-backend-0.1.0.0-IU5LaHcdXoz65g8YnoPRFA:Ohua.Backend.Normalize
-        -}  
-        it "Assignment and return" $
-            (showCode "Compiled: " =<< compileCode Input.emptyReturn) >>=
-            (\compiled -> do
-                expected <- showCode "Expected:" Expect.emptyReturn
-                compiled `shouldBe` expected)
-
-        {-
-        uncaught exception: ErrorCall
-        Internal Error: Tried running substitution on sending Binding "x" with not yet supported task expression: Lit UnitLit
-        CallStack (from HasCallStack):
-             error, called at src/Ohua/Backend/Normalize.hs:83:12 in ohua-backend-0.1.0.0-IU5LaHcdXoz65g8YnoPRFA:Ohua.Backend.Normalize
-        -}
-        it "Assignment and return None" $
-            (showCode "Compiled: " =<< compileCode Input.noneReturn) >>=
-            (\compiled -> do
-                expected <- showCode "Expected:" Expect.noneReturn
-                compiled `shouldBe` expected)
-        
-        {-
-        uncaught exception: ErrorCall
-        Internal Error: Tried running substitution on sending Binding "x" with not yet supported task expression: Lit UnitLit
-        CallStack (from HasCallStack):
-             error, called at src/Ohua/Backend/Normalize.hs:83:12 in ohua-backend-0.1.0.0-IU5LaHcdXoz65g8YnoPRFA:Ohua.Backend.Normalize
-        -}
-        it "Expression no Return" $
-            (showCode "Compiled: " =<< compileCode Input.exprNoReturn) >>=
-            (\compiled -> do
-                expected <- showCode "Expected:" Expect.exprNoReturn
-                compiled `shouldBe` expected)
-
-        -- [Error] Compiler invariant broken! Seq must have two inputs where the second is a literal:
-        -- PureDFFun (Direct (DataBinding (Binding "a_0"))) (QualifiedBinding (NSRef [Binding "ohua",Binding "lang"]) (Binding "seq")) (DFEnvVar TypeVar (NumericLit 5) :| [DFEnvVar TypeVar UnitLit])
-        it "Assignment numeric Literal" $
-            (showCode "Compiled: " =<< compileCode Input.assignNumLit) >>=
-            (\compiled -> do
-                expected <- showCode "Expected:" Expect.assignNumLit
-                compiled `shouldBe` expected)      
-        -- [Error] Expected var or apply expr, got Lit (NumericLit 5)
-        -- CallStack (from HasCallStack):
-        --throwErrorS, called at src/Ohua/Core/Util.hs:228:20 in ohua-core-0.3.0-k6lwMxcZZw8E652k33pRb:Ohua.Core.Util
-        --throwErrorDebugS, called at src/Ohua/Core/Monad.hs:39:12 in ohua-core-0.3.0-k6lwMxcZZw8E652k33pRb:Ohua.Core.Monad
-        -- failWith, called at src/Ohua/Core/Compile.hs:99:25 in ohua-core-0.3.0-k6lwMxcZZw8E652k33pRb:Ohua.Core.Compile
-        it "Assignment numeric Literal and return var" $
-            (showCode "Compiled: " =<< compileCode Input.assignNumLitReturn) >>=
-            (\compiled -> do
-                expected <- showCode "Expected:" Expect.assignNumLitReturn 
-                compiled `shouldBe` expected)     
-        
-        
-        -- Same Error as with all 'None Return tests'
-        it "Assignments, Call function, Return Var" $
-            (showCode "Compiled: " =<< compileCode Input.assignmentCallReturn) >>=
-            (\compiled -> do
-                expected <- showCode "Expected:" Expect.assignmentCallReturn
-                compiled `shouldBe` expected)
-
         
         {-
         -- Fails until I implement augmented assignments
@@ -214,4 +153,3 @@ spec =
                 expected <- showCode "Expected:" Expect.assignAugmented
                 compiled `shouldBe` expected)
         -}
--}
