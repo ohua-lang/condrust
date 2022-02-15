99883815b1c3d77dcfa36ecc18dcaaa860ec6cee
Felix Wittwer <dev@felixwittwer.de>
2022-02-15 16:36:56 +0100

    Add python and Rust packages

--- shell.nix
+++ shell.nix
@@ -1,6 +1,14 @@
 let pkgs = import <nixpkgs> {};
 in pkgs.mkShell {
   propagatedBuildInputs = with pkgs; [
+    # the actual haskell toolstack
     stack
+
+    # Rust Compiler
+    cargo
+
+    # python runtime
+    pkgs.python3Full
+    pkgs.python3Packages.multiprocess
   ];
 }
