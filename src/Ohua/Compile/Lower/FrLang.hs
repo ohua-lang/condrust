d65bf1bac14f74583c0c82e0728e4d7ad7e270dd
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2022-08-16 17:08:04 +0200

    preliminary while conversion
    We want to convert while loops from the
    frontend integrations into recursions
    supported by the core.
    We want to do this based on the FrLang
    such that integration devs do not need to figure
    out on their own.
    
    The function/pass doing so isn't ready yet, see
    code comments

--- src/Ohua/Compile/Lower/FrLang.hs
+++ src/Ohua/Compile/Lower/FrLang.hs
@@ -8,7 +8,7 @@ import Data.Functor.Foldable (cata)
 import qualified Data.HashSet as HS
 import GHC.Exts
 
-import Ohua.Frontend.Lang
+import Ohua.Frontend.Lang as FR
 import Ohua.Frontend.PPrint ()
 import Ohua.Core.ALang.Lang hiding (Expr, ExprF)
 import qualified Ohua.Core.ALang.Lang as AL
@@ -16,27 +16,27 @@ import qualified Ohua.Core.ALang.Refs as ARefs
 import Ohua.Core.ParseTools.Refs (ifBuiltin, mkTuple, smapBuiltin, seqBuiltin)
 
 
--- | Not sure this traversal is necessary, but it makes every smap argument into
+-- | Not sure this traversal is necessary, but it turns every smap argument into
 -- a lambda
 --
 -- I am leaving it here in case we need it later.
-_ensureLambdaInSmap :: (Monad m, MonadGenBnd m) => Expr ty -> m (Expr ty)
+_ensureLambdaInSmap :: (Monad m, MonadGenBnd m) => FR.Expr ty -> m (FR.Expr ty)
 _ensureLambdaInSmap =
     rewriteM $ \case
-        MapE (LamE _ _) _ -> pure Nothing
+        MapE (FR.LamE _ _) _ -> pure Nothing
         MapE other coll -> do
             bnd <- generateBinding
             pure $ Just $ MapE (LamE [VarP bnd] $ AppE other [VarE bnd]) coll
         _ -> pure Nothing
 
 -- | Ensures every lambda takes at most one argument.
-mkLamSingleArgument :: Expr ty -> Expr ty
+mkLamSingleArgument :: FR.Expr ty -> FR.Expr ty
 mkLamSingleArgument =
     rewrite $ \case
         LamE (x1:x2:xs) b -> Just $ LamE [x1] $ LamE (x2 : xs) b
         _ -> Nothing
 
-removeDestructuring :: MonadGenBnd m => Expr ty -> m (Expr ty)
+removeDestructuring :: MonadGenBnd m => FR.Expr ty -> m (FR.Expr ty)
 removeDestructuring =
     rewriteM $ \case
         LetE (TupP pats) e1 e2 -> do
@@ -47,16 +47,97 @@ removeDestructuring =
             pure $ Just $ LamE [VarP valBnd] $ unstructure valBnd pats e
         _ -> pure Nothing
 
-giveEmptyLambdaUnitArgument :: Expr ty -> Expr ty
+giveEmptyLambdaUnitArgument :: FR.Expr ty -> FR.Expr ty
 giveEmptyLambdaUnitArgument =
     rewrite $ \case
         LamE [] e -> Just $ LamE [UnitP] e
         _ -> Nothing
 
-nthFun :: Expr ty
+-- | The idea here is, that we transform a while loop to a recursive call
+--   As the inner part of the while loop is a local scope and there is no 'return' 
+--   for local variables, the arguments as well as the return of the recursive call are 
+--   (stateful) objects from the outer context, that are manipulated/used in the loop.
+--   Assuming we had assignments in the supported subset, we'd also need to scan for variables that
+--   are assigned in the scope e.g.
+{- fun(...){
+    ...
+    while state1.next(){
+        let e = state1.get();
+        state.do_stuff(e)
+    }
+   }
+
+   should become:
+
+    fun(...){
+    ...
+    let (state1, state2) = recursive_while(state1, state2) 
+   }
+-} 
+-- QUESTION: Do we need checks for validity here that arrise purely from the conversion? 
+--           We do not care (at all) for invalid Code of the input language, and should treat 
+--           more general problems that affect also recursions downstream.
+
+whileToRecursion :: MonadGenBnd m =>  FR.Expr ty -> m (FR.Expr ty)
+whileToRecursion = 
+    rewriteM $ \case
+        WhileE cond body -> Just <$> do
+            loopName <- generateLoopName body
+            let (branchingFunction, stateVars) = generateIfSplit loopName cond body
+            let whileLoopFunction = (trace $ "WHILE TRANSFORM: "<> show stateVars) 
+                                     LamE (map VarP stateVars) $ StmtE body branchingFunction 
+            return $ LetE (VarP loopName) whileLoopFunction branchingFunction
+        _ -> return Nothing
+
+        where
+            generateLoopName :: MonadGenBnd m => FR.Expr ty -> m Binding
+            generateLoopName _b = generateBindingWith "while_loop_function"
+
+            generateIfSplit :: Binding -> FR.Expr ty -> FR.Expr ty -> (FR.Expr ty, [Binding])
+            generateIfSplit loopName cond body = 
+                -- We need to pass statefull variables from one recursion to the next.
+                -- This may include a stateful varaible used in the condition e.g. while iter.has_next()
+                let usedStates = extractUsedStates body
+                    returnTuple = 
+                        -- ToDo: Tuple kommt nicht mit nur einem Element klar -> 1. adapt with check -> Fix later
+                        TupE (map VarE usedStates)
+                    recCall = 
+                        LetE "bla" returnTuple $
+                        IfE cond (AppE (VarE loopName) (map VarE usedStates)) (VarE "bla")
+                    -- recCall = IfE cond (AppE (VarE loopName) usedStates) usedStates
+                in (recCall, usedStates)
+                
+            extractUsedStates ::  FR.Expr ty -> [Binding]
+            -- ToDo: 1. BindE expr expr might 'bind' other expression as well. For now we want to error in that case 
+            extractUsedStates body = map 
+                (\e -> case e of 
+                    VarE bnd -> bnd
+                    expr -> error $ "We currently only support stateful functions directly called on objects. Please rewrite for " <> show expr) 
+                [expr | BindE expr _funE <- universe body]
+
+
+ {- Original While Transformation in the frontend. I suspect I can/should replace the empty
+    args with the actual states beeing processed in the loop.
+
+    let loopLambdaRef = "while_loop_body"
+    let recur =
+          IfE
+            cond'
+            (AppE (VarE loopLambdaRef)  [])
+            $ LitE UnitLit
+    return $
+      -- FIXME proper name generation needed here!
+      LetE
+        (VarP loopLambdaRef)
+        (LamE [] $ StmtE block' recur)
+        recur
+    -}
+
+
+nthFun :: FR.Expr ty
 nthFun = LitE $ FunRefLit $ FunRef ARefs.nth Nothing $ FunType $ Right $ TypeVar :| [TypeVar,TypeVar]
 
-unstructure :: Binding -> [Pat] -> Expr ty -> Expr ty
+unstructure :: Binding -> [FR.Pat] -> FR.Expr ty -> FR.Expr ty
 unstructure valBnd pats = go (toInteger $ length pats) pats
   where
     go numPats =
@@ -72,7 +153,7 @@ unstructure valBnd pats = go (toInteger $ length pats) pats
                      ]) .
         zip [0 ..]
 
-trans :: Expr ty -> AL.Expr ty
+trans :: FR.Expr ty -> AL.Expr ty
 trans =
     cata $ \case
         VarEF b -> Var b
@@ -96,26 +177,33 @@ trans =
         BindEF ref e -> BindState ref e
         StmtEF e1 cont -> Let "_" e1 cont
         SeqEF source target -> seqBuiltin `Apply` source `Apply` target
+        -- QUESTION: Where should this come from.
+        -- ISSUE: TypeVar may cause problems for Backends where 'type variables' are just not an option
         TupEF parts -> foldl Apply (pureFunction mkTuple Nothing $ FunType $ Right $ TypeVar :| [TypeVar]) parts
+        WhileEF cond _body ->  error "While loop has not been replaced. Please file a bug"
   where
     patToBnd =
         \case
             VarP v -> v
             UnitP -> "_"
-            p -> error $ "Invariant broken, invalid pattern: " <> show p
+            p -> error $ 
+             "Compiler screwed up. At this point any patterns" <>
+             "(function arguments or let bound variables) should be single vars or wild card but " <> show p <>
+             "is not. Please file a bug."
 
 toAlang' :: CompM m => HS.HashSet Binding -> Expr ty -> m (AL.Expr ty)
 toAlang' taken expr = runGenBndT taken $ transform expr
     where 
         transform =
+            whileToRecursion >=>
             giveEmptyLambdaUnitArgument >>>
             mkLamSingleArgument >>> 
             removeDestructuring >=> pure . trans
 
-toAlang :: CompM m => Expr ty -> m (AL.Expr ty)
-toAlang expr = toAlang' (definedBindings expr) expr
+toAlang :: CompM m => FR.Expr ty -> m (AL.Expr ty)
+toAlang expr = (trace $ "Converting to Alang, Bindings are" <> show (definedBindings expr)) toAlang' (definedBindings expr) expr
 
-definedBindings :: Expr ty -> HS.HashSet Binding
+definedBindings :: FR.Expr ty -> HS.HashSet Binding
 definedBindings olang =
     HS.fromList $
     [v | VarE v <- universe olang] <>
