d419f9c2ea6a34cd8ba7bea2262c28dede70b13b
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2022-09-11 10:29:54 +0200

    adding returnType field to Algo

--- src/Ohua/Types/Integration.hs
+++ src/Ohua/Types/Integration.hs
@@ -23,27 +23,53 @@ declareLenses[d|
           deriving (Show, Eq)
     |]
 
+-- REMINDER: As long as not all expressions are typed and as long as therefore Ohua nodes are introduced with type
+-- variables and as long as therefore I need to have a type propagation pass to type them....As long as this is the
+-- case I need an extra field in the Algo definition to carry through the return type of the original code.  
+-- Of course, we allready have this information present in the `algoInputCode`, but we can not use in the middle of the
+-- compiler because a) there we don't know the concrete language we're compiling and hence not the patterns 
+-- to match on to extract the type and b) we'd carry lots of stuff through the compile steps when all we need is one type.
+
 declareLenses[d|
-     data Algo expr anno = Algo 
+     data Algo expr inpt retTy= Algo 
           { algoName :: Binding
           , algoCode :: expr
-          , algoAnno :: anno
+          , algoInputCode :: inpt
+          , algoReturnTy :: retTy
           } deriving (Show, Eq)
     |]
 
+
+-- | A Namespace consists of a reference, a list of Imports and a list of Algorithms extracted from a given 
+--   input file. Each Algorithm in turn consists of the `algoCode`, the extracted/current compiler internal representation
+--   of the algorithm and (maybe not perfectly named though) the `algoInputCode` carrying the original code of 
+--   each compiled function.
 declareLenses[d|
-     data Namespace expr anno = Namespace 
+     data Namespace expr inpt retTy = Namespace 
           { nsName :: NSRef
           , imports :: [Import]
-          , algos :: [Algo expr anno]
+          , algos :: [Algo expr inpt retTy]
           } deriving (Show, Eq)
     |]
 
-updateExprs :: Monad m => Namespace expr1 anno -> (expr1 -> m expr2) -> m (Namespace expr2 anno)
+updateExprs :: Monad m => Namespace expr1 inpt retTy -> (expr1 -> m expr2) -> m (Namespace expr2 inpt retTy)
 updateExprs namespace f = updateExprs' namespace $ \_ e -> f e
 
-updateExprs' :: Monad m => Namespace expr1 anno -> (Binding -> expr1 -> m expr2) -> m (Namespace expr2 anno)
+updateExprs' :: Monad m => Namespace expr1 inpt retTy -> (Binding -> expr1 -> m expr2) -> m (Namespace expr2 inpt retTy)
 updateExprs' namespace f = do
+     algos' <- 
+          forM (namespace^.algos) $ \algo -> do
+               algoCode' <- f (algo^.algoName) (algo^.algoCode)
+               return $ over algoCode (const algoCode') algo
+     return $ over algos (const algos') namespace
+
+
+
+updateExprsWithAnno :: Monad m => Namespace expr1 inpt retTy-> (expr1 -> m expr2) -> m (Namespace expr2 inpt retTy)
+updateExprsWithAnno namespace f = updateExprs' namespace $ \_ e -> f e
+
+updateExprsWithAnno' :: Monad m => Namespace expr1 inpt retTy -> (Binding -> expr1 -> m expr2) -> m (Namespace expr2 inpt retTy)
+updateExprsWithAnno' namespace f = do
      algos' <- 
           forM (namespace^.algos) $ \algo -> do
                algoCode' <- f (algo^.algoName) (algo^.algoCode)
