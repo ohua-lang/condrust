21cb3e5e572589beae1350e028e608896bdd4572
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2022-11-04 08:42:59 +0100

    remove printouts

--- test/Integrations/Python/PythonSetup.hs
+++ test/Integrations/Python/PythonSetup.hs
@@ -74,13 +74,13 @@ compileModule inCode opts compType = do
                         $ compile inFile compScope options integrationOptions outDir
                     (caller:modules) <- listDirectory outDir
                     -- putStr filesHint
-                    mapM_ putStr (caller:modules)
+                    -- mapM_ putStr (caller:modules)
                     -- files <- mapM (\name -> readFile (outDir </> name)) (caller:modules)
-                    placeholderFile <-readFile (outDir </> "placeholderlib.py")
-                    putStr placeholderFile
+                    -- placeholderFile <-readFile (outDir </> "placeholderlib.py")
+                    -- putStr placeholderFile
                     producedFile <-readFile (outDir </> "algo.py")
                     -- newMain <-readFile (outDir </> "test.py")
-                    --putStr newMainStr
+                    -- putStr newMainStr
                     -- putStr $ newMain  <> "\n"
                     -- putStr algoModStr 
                     -- putStr producedFile
