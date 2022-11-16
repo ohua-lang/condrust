8ddef6923c2b97d7ba2249e9cb8c47c8d8d66a37
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2022-11-16 19:26:46 +0100

    support destruct > 2
    This just adds a little helper function needed to convert the
    new `Indexing "someTuple" 0` in the backend

--- src/Ohua/Types/Vector.hs
+++ src/Ohua/Types/Vector.hs
@@ -132,10 +132,15 @@ withSuccSing :: Nat -> (forall n.SNat ('Succ n) -> a) -> a
 withSuccSing n f = case toSing n of
                      SomeSing s@(SSucc _) -> f s
                      
-
 natToInt :: Nat -> Int
 natToInt Zero = 0
 natToInt (Succ n) = 1 + natToInt n
+
+intToNat :: Integer -> Nat
+intToNat x 
+ | x == 0 = Zero
+ | x > 0 = Succ (intToNat (x-1))
+ | otherwise = undefined
 
 instance Traversable (Vec n ) where
   traverse f VNil = pure VNil
