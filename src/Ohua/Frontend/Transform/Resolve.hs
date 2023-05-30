dfa85aa89237a7755b4f84639ef0408109cacb6f
Pagolin <lisza.zeidler@tu-dresden.de>
2023-05-30 18:50:18 +0200

    save algo types druing inlining

--- src/Ohua/Frontend/Transform/Resolve.hs
+++ src/Ohua/Frontend/Transform/Resolve.hs
@@ -32,7 +32,7 @@ import qualified Data.Text as T
 
 -- REMINDER: Algo Inlining happens here. If function types from algos shall be preserved, check resolveExpr function
 -- | This function basically inlines function calls from inside the compile scope. It goes over all
---   algorithms loaded from the original file, extracts function calls from their code, retrievs the
+--   algorithms loaded from the original file, extracts function calls from their code, retrieves the
 --   code of the called functions from the registry and replaces a
 --   @let x = fun(arg)@ 
 --   by 
@@ -47,48 +47,48 @@ resolveNS (ns, registry) =
     return $ over algos (map (\algo -> over algoCode (work $ view algoName algo) algo)) ns
     where
         work :: Binding -> Expr ty -> Expr ty
-        work name e =
+        work algoNm algoExpr =
             let
-                calledFunctions = collectAllFunctionRefs name registry e
-                expr = foldl (flip addExpr) e calledFunctions
-                e' = resolveExpr expr
-            in e'
+                calledFunctions = collectAllFunctionRefs algoNm registry algoExpr
+                expr = foldl (flip addExpr) algoExpr calledFunctions
+                algoExpr' = resolveExpr expr
+            in algoExpr'
 
-        collectAllFunctionRefs :: Binding -> NamespaceRegistry ty -> Expr ty -> HS.HashSet QualifiedBinding
+        collectAllFunctionRefs :: Binding -> NamespaceRegistry ty -> Expr ty -> HS.HashSet (QualifiedBinding, FunType ty)
         collectAllFunctionRefs name available =
             HS.unions .
             HS.toList . 
-            HS.map (\qb ->
+            HS.map (\(qb, ty) ->
                 maybe 
                     HS.empty
-                    (HS.insert qb . collectAllFunctionRefs name (HM.delete qb available))
+                    (HS.insert (qb, ty) . collectAllFunctionRefs name (HM.delete qb available))
                     $ HM.lookup qb available) . 
-            HS.filter (/= QualifiedBinding (makeThrow []) name) .
-            collectFunctionRefs
+            HS.filter (\(qb, ty )-> qb /= QualifiedBinding (makeThrow []) name) .  collectFunctionRefs
 
-        collectFunctionRefs :: Expr ty -> HS.HashSet QualifiedBinding
+        collectFunctionRefs :: Expr ty -> HS.HashSet (QualifiedBinding, FunType ty)
         collectFunctionRefs e =
             -- FIXME we need to resolve this reference here against the namespace and the registry (for Globs).
-            HS.fromList [r | LitE (FunRefLit (FunRef r _ _)) <- universe e]
+            HS.fromList [(r, fTy) | LitE (FunRefLit (FunRef r _ fTy)) <- universe e]
         
         pathToVar :: QualifiedBinding -> Binding
         pathToVar (QualifiedBinding ns bnd) = 
             (makeThrow . T.intercalate ".") $ map unwrap $ unwrap ns ++ [bnd]
         
-        addExpr :: QualifiedBinding -> Expr ty -> Expr ty
+        addExpr :: (QualifiedBinding, FunType ty) -> Expr ty -> Expr ty
         -- TODO This is an assumption that fails in Ohua.Compile.Compiler.prepareRootAlgoVars
         --      We should enforce this via the type system rather than a runtime error!
-        addExpr bnd (LamE vars body) = LamE vars $ addExpr bnd body
-        addExpr bnd e = 
+        addExpr (otherAlgo, fTy) (LamE vars body) = LamE vars $ addExpr  (otherAlgo, fTy) body
+        addExpr (otherAlgo, fTy) e = 
+            -- (trace $"Adding Expression. Assign bind : "<> show bnd <> "\n to expression: "<> quickRender e)
             LetE
-                (VarP (pathToVar bnd) TypeVar)
+                (VarP (pathToVar otherAlgo) (TypeFunction fTy))
                 (fromMaybe 
                     (error "impossible") -- the path was originally retrieved from this list
-                    $ HM.lookup bnd registry)
+                    $ HM.lookup otherAlgo registry)
                 e
 
         -- turns the function literal into a simple (var) binding
         resolveExpr :: Expr ty -> Expr ty
         resolveExpr = cata $ \case
-            LitEF (FunRefLit (FunRef ref _id _)) | HM.member ref registry -> VarE (pathToVar ref) TypeVar
+            LitEF (FunRefLit (FunRef fName _id funTy)) | HM.member fName registry -> VarE (pathToVar fName) (TypeFunction funTy)
             e -> embed e
