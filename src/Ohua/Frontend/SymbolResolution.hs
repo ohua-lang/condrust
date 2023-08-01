dc9f7052d81fe17ba0730bb8e34a68f2af5c9419
Pagolin <lisza.zeidler@tu-dresden.de>
2023-08-01 11:27:14 +0200

    renaming/exports

--- src/Ohua/Frontend/SymbolResolution.hs
+++ src/Ohua/Frontend/SymbolResolution.hs
@@ -1,9 +1,14 @@
-module Ohua.Frontend.SymbolResolution (resolveSymbols) where
+module Ohua.Frontend.SymbolResolution (
+  resolveSymbols
+  , SymResError(..)
+  , Delta
+  , Gamma
+  ) where
 
-import Ohua.UResPrelude
-import Ohua.Frontend.Lang as FrLang
+import Ohua.UResPrelude hiding (alias)
 import qualified Ohua.Prelude as Res (FunType(..))
 import qualified Data.HashMap.Lazy as HM
+import qualified Data.List.NonEmpty as NE
 
 data SymResError
   = BndError Binding
@@ -19,26 +24,26 @@ Gamma ... associates local variables to a type
 Delta ... associates function literals to a type
 -}
 
-type Gamma varTy ty = HM.HashMap Binding (varTy ty)
+type Gamma ty res = HM.HashMap Binding (OhuaType ty res)
 -- Question: Curent representation of FunType allows Unresolved. DO we want to forbid this here?
 type Delta ty res = HM.HashMap QualifiedBinding (Res.FunType ty res)
 
 
-resolve :: Delta ty res -> [Import] -> Maybe NSRef -> Binding -> Either (QualifiedBinding, FunType ty res ) SymResError
-resolve delta imports (Just ns) bnd =
-  let qb = resolveQBnd imports $ QualifiedBinding ns bnd
+resolveSymbols :: Delta ty res -> [Import] -> Maybe NSRef -> Binding -> Either (QualifiedBinding, FunType ty res ) SymResError
+resolveSymbols delta mod_imports (Just nspace) bnd =
+  let qb = resolveQBnd mod_imports $ QualifiedBinding nspace bnd
   in case HM.lookup qb delta of
        Just t -> Left (qb, t)
        Nothing -> Right $ QBndError qb
-resolve delta imports Nothing bnd =
-  case resolveBnd imports bnd of
+resolveSymbols delta mod_imports Nothing bnd =
+  case resolveBnd mod_imports bnd of
     Left qbs -> check delta qbs
     Right r -> Right r
 
 check :: Delta ty res -> NonEmpty QualifiedBinding -> Either (QualifiedBinding, FunType ty res) SymResError
 check delta (qb:|[]) =
   case HM.lookup qb delta of
-    Just t -> Left (qb t)
+    Just t -> Left (qb, t)
     Nothing -> Right $ NoTypeFound qb
 check delta (qb:|(qbs:qbss)) =
   case HM.lookup qb delta of
@@ -50,17 +55,17 @@ check delta (qb:|(qbs:qbss)) =
 
 resolveQBnd :: [Import] -> QualifiedBinding -> QualifiedBinding
 resolveQBnd [] qb = qb
-resolveQBnd ((Alias ns alias):is) (QualifiedBinding ns' b) | NSRef [alias] == ns' = QualifiedBinding ns b
-resolveQBnd (_:is) qb = resolveQB is qb
+resolveQBnd ((Alias nspace alias): _is) (QualifiedBinding nspace' b) | NSRef [alias] == nspace' = QualifiedBinding nspace b
+resolveQBnd (_:is) qb = resolveQBnd is qb
 
 resolveBnd :: [Import] -> Binding -> Either (NonEmpty QualifiedBinding) SymResError
 resolveBnd [] bnd = Right $ BndError bnd
-resolveBnd ((Full ns bnd'):is) bnd | bnd' == bnd =
+resolveBnd ((Full nspace bnd') : is) bnd | bnd' == bnd =
                                      case resolveBnd is bnd of
-                                       Left other -> Left (qb <| other)
-                                       Right _ -> Left (QualifiedBinding ns bnd :| [])
-resolveBnd ((Glob ns):is) bnd =
+                                       Left other -> Left ( QualifiedBinding nspace bnd NE.<| other)
+                                       Right _ -> Left (QualifiedBinding nspace bnd :| [])
+resolveBnd ((Glob nspace):is) bnd =
   case resolveBnd is bnd of
-    Left other -> Left (QualifiedBinding ns bnd <| other)
-    Right _ -> Left (QualifiedBinding ns bnd :| [])
+    Left other -> Left (QualifiedBinding nspace bnd NE.<| other)
+    Right _ -> Left (QualifiedBinding nspace bnd :| [])
 resolveBnd (_:is) bnd = resolveBnd is bnd
