21384bbc23c3920a30c5644f4824efe0e6a5d921
Pagolin <lisza.zeidler@tu-dresden.de>
2023-09-11 09:26:00 +0200

    integrations compiles again

--- src/Ohua/Integration/Rust/Architecture/M3.hs
+++ src/Ohua/Integration/Rust/Architecture/M3.hs
@@ -51,9 +51,9 @@ convertChan rxMutability (SRecv _ty (SChan bnd)) =
 convertReceive :: Binding -> Com 'Recv TH.RustVarType -> Sub.Expr
 convertReceive suffix (SRecv argType (SChan channel)) =
   let ty' = case argType of
-              (Type (HostType( TH.Self ty _ _mut))) -> noSpan <$ ty
+              (HType (HostType( TH.Self ty _ _mut)) _ ) -> noSpan <$ ty
               -- error "Not yet implemented: Recv of reference type"
-              (Type (HostType(TH.Normal ty))) -> noSpan <$ ty
+              (HType (HostType(TH.Normal ty)) _ ) -> noSpan <$ ty
               internalType -> case convertToRustType internalType of 
                   Just (Sub.RustType ty) -> ty
                   Nothing -> error $ "Couldn't handle type " <> show argType <> " for channel " <> show channel
