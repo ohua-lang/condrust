e24027951365d8990e021a55472964a8c93dfe3f
Pagolin <lisza.zeidler@tu-dresden.de>
2023-11-03 09:49:43 +0100

    comments explaining flow

--- src/Ohua/Frontend.hs
+++ src/Ohua/Frontend.hs
@@ -22,17 +22,25 @@ frontend :: forall m lang. (ErrAndLogM m, Integration lang)
          -> FilePath
          -> m ((HostModule lang, Namespace (FrLang.Expr (Type lang) Resolved) (AlgoSrc lang)), HostModule lang)
 frontend lang compScope inFile = do
+        -- 1. Extract: (input file in host language,
+        --              Integration.Namespace containing Integration.Algo's, Integration.Imports's and Integration.Global's of the input module,
+        --              the 'register' a map {algo_name: lowered code} used for inlining the algorithms,
+        --              placeholder (potentialy) containing host language code we "wrapped" out of the input code because we cannot compile it)
         (langNs, ns, reg, placeholder) <- loadAlgosAndImports lang compScope inFile
+        -- make sure all functions have at least a Unit argument and no algo returns a literal 
         nsTransf                       <- updateExprs ns trans
+        -- Inline algo code from the register
         nsReslvd                       <- resolveNS (nsTransf,reg)
+        -- Extract Delta i.e. the types of all functions used in the algos as {function_name:type}
         delta                          <- loadTypes lang langNs nsReslvd
-
-        -- we exclude recursive functions from stand-alone compilation.
+        -- Exclude recursive functions from stand-alone compilation.
         let nsNoRecs                      =  over algos (filter (not . isRecAlgo)) nsReslvd
         -- FIXME: We ignore global definitions from the Namespace here
+        -- Check typing of algorithms and propagate type information from Delta 
         nsWellTypd                        <- updateExprs nsNoRecs (toWellTyped delta (nsNoRecs^.imports))
+        -- Transform algos with multiple arguments to nested one argument functions
         nsLetRoot                         <- updateExprs nsWellTypd prepareRootAlgoVars
-        return ((langNs, nsLetRoot ), placeholder)
+        return ((langNs, nsLetRoot), placeholder)
     where
         trans :: ErrAndLogM m => FrLang.Expr ty Unresolved -> m (FrLang.Expr ty Unresolved)
         trans e = noFinalLiterals e >> noEmptyArgs e
